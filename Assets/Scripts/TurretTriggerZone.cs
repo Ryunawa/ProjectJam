958efeef4f74a1cdb72de4c4e8287f8612c7b942
Robin Douet <robin.douet@gmail.com>
2022-04-09 21:45:15 +0200

    Revert "Animation + instance arrow sur archers"
    This reverts commit a816c2c2364988d20a5898e8626be2ffa5fcfa4c.

--- Assets/Scripts/TurretTriggerZone.cs
+++ Assets/Scripts/TurretTriggerZone.cs
@@ -5,31 +5,22 @@ using UnityEngine;
 public class TurretTriggerZone : MonoBehaviour
 {
     public float detectionRange = 20.0f;
-
-    private SquirrelBehaviour _SquirrelTurr;
+   
     private TurretBehaviour _turrScript;
     private Transform _player;
 
-    void Start()
+    private void Start()
     {
         _player = GameObject.Find("Player").transform;
-        _turrScript = GetComponent<TurretBehaviour>();
-        _SquirrelTurr = GetComponent<SquirrelBehaviour>();
+        _turrScript = this.GetComponent<TurretBehaviour>();
     }
 
-    void Update()
+    private void Update()
     {
         float distance = Vector2.Distance(_player.transform.position, this.transform.position);
         if (distance <= detectionRange)
-        {
             _turrScript.IsShooting = true;
-            _SquirrelTurr.StartAnimationShooting(true);
-        }
         else
-        {
             _turrScript.IsShooting = false;
-            _SquirrelTurr.StartAnimationShooting(false);
-
-        }
     }
 }
