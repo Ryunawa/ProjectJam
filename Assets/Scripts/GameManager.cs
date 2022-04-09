96a821e93c0a735ed4b78bc716377d72965677f5
Robin Douet <robin.douet@gmail.com>
2022-04-09 12:42:34 +0200

    Fix scale + respawn added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,27 +10,41 @@ public class GameManager : MonoBehaviour
 
     private static GameManager _instance;
         
-        public static GameManager Instance => _instance;
-    
-        private void Awake()
-        {
-            if(_instance == null)
-            {
-                _instance = this;
-                _instance.Setup();
-                DontDestroyOnLoad(gameObject);
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
-        }
+    public static GameManager Instance => _instance;
 
-        #endregion
-
-        private void Setup()
+    public GameObject respawnPoint;
+    public GameObject player;
+    private void Awake()
+    {
+        if(_instance == null)
         {
-            throw new NotImplementedException();
+            _instance = this;
+            _instance.Setup();
+            DontDestroyOnLoad(gameObject);
         }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
+    void Start()
+    {
+        player.GetComponent<Transform>();
+        respawnPoint.GetComponent<Transform>();
+            ;
+    }
+
+    private void Setup()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Respawn()
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
     
 }
