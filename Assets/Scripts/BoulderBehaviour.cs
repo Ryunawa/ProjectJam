a0f87e7e3b1aa49bfcb8b98dde4536ee968fcdf8
floriandjomain <45943329+floriandjomain@users.noreply.github.com>
2022-04-09 14:25:51 +0200

    The magic boulder works

---
+++ Assets/Scripts/BoulderBehaviour.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class BoulderBehaviour : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private GameObject _boulderDestroyer;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject == _boulderDestroyer)
+        {
+            ResetPos();
+        }
+    }
+
+    private void ResetPos()
+    {
+        transform.position = _spawnPoint.position;
+    }
+}
